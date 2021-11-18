959e66cdd35596b158452bfd25dbbe4c65e6f07b
aserkanyilmaz <sekoylmz14@gmail.com>
2021-11-18 18:08:11 +0300

    ok

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -38,7 +38,52 @@ namespace WebApi.AddControllers{
             var bookList = BookList.OrderBy(x=>x.Id).ToList<Book>();
             return bookList;
         }
+        [HttpGet("{id}")]    
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id ).SingleOrDefault();
+            return book;
+        }
 
+        [HttpPost]
+
+        public IActionResult AddBook([FromBody] Book newBook )
+        {
+            var book = BookList.SingleOrDefault(x=>x.Title == newBook.Title);
+
+            if(book is not null)
+                return BadRequest();
+            
+            BookList.Add(newBook);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x=>x.Id==id);
+
+            if(book is null)
+                return BadRequest();
+            
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+
+            return  Ok();
+        }
+        [HttpDelete("{id")]
+
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x=> x.Id ==id);
+            if(book is null)
+                return BadRequest();
+
+            BookList.Remove(book);
+            return Ok();    
+        }
 
     }
 }
